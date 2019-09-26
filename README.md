d5d11d3e311c8c8889a14bb5df7336ef612767c6
Aitor Ruiz <aitorruiz2m@gmail.com>
2019-09-26 10:08:44 +0200

    Mejora general

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Proyecto para administrar una red de drones de reparto a nivel de España.
 ![Alt text](mapa.PNG?raw=true "Title")
 Usando una imagen de OpenStreetMaps vamos a hacer una simulacion de donde estan los drones en este momento.
 ![Alt text](14466.png?raw=true "Title")
+
 Con un modelo de un dron basico lo moveremos por el mapa simulando el paso del tiempo
 ## Administracion (Parte derecha)
 En la parte derecha de la interfaz hay una serie de desplegables para establecer una ruta
--- drones/src/main/java/proyectoUniversidad/App.java
+++ drones/src/main/java/proyectoUniversidad/App.java
@@ -3,6 +3,10 @@ package proyectoUniversidad;
 //import javax.swing.JFrame;
 
 import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 /**
  * Creacion de ventana
  *
@@ -13,10 +17,18 @@ public class App extends JFrame {
 	 *
 	 */
 	private static final long serialVersionUID = 1L;
-	public static void main( String[] args )
-    {
+	public static void main( String[] args ) {
         App aplicacion = new App();
-        aplicacion.crearVentana();
+        //aplicacion.crearVentana();
+        File sourceFile = new File("directory-source/text1.txt");
+		File destinationFile = new File("directory-destination/test1.txt");
+
+        try {
+			FileUtils.moveFile(sourceFile, destinationFile);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
     public void crearVentana(){
         this.setSize(400,400);
