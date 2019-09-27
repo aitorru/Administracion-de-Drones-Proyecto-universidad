9bb3c193ca93c73a1f5f19fa9f94daf7c337de4f
Aitor Ruiz <aitorruiz2m@gmail.com>
2019-09-27 11:41:45 +0200

    Añadido dependencias de JSON y eliminados los jar del git

--- drones/src/main/java/com/AdministracionDrones/App.java
+++ drones/src/main/java/com/AdministracionDrones/App.java
@@ -1,39 +1,33 @@
 package com.AdministracionDrones;
 
-//import javax.swing.JFrame;
-
 import javax.swing.*;
-import java.io.File;
-import java.io.IOException;
 
-import org.apache.commons.io.FileUtils;
+
 /**
  * Creacion de ventana
  *
  *
  */
-public class App extends JFrame {
+public class App extends JFrame{
+    
     /**
-	 *
-	 */
-	private static final long serialVersionUID = 1L;
-	public static void main( String[] args ) {
-        App aplicacion = new App();
-        aplicacion.crearVentana();
-        File sourceFile = new File("text1.txt");
-		File destinationFile = new File("test1.txt");
+     *
+     */
+    private static final long serialVersionUID = 1L;
 
-        try {
-            FileUtils.moveFile(sourceFile, destinationFile);
-            System.out.println("Movido");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                App a = new App();
+                a.crearVentana();
+            }
+        });
     }
     public void crearVentana(){
         this.setSize(400,400);
+        this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
         this.setVisible(true);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
     }
 }
