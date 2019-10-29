f3f7689d8f68e86a6ad56f30110fd656f26a7a9e
ssc10 <ssc10@DESKTOP-6JKN4DC>
2019-10-29 09:39:16 +0100

    Vector class for the drone movement implemented

---
+++ drones/src/main/java/com/AdministracionDrones/Vector.java
@@ -0,0 +1,24 @@
+
+public class Vector {
+
+
+	 public float x;
+	    public float y;
+
+	    public Vector(float x, float y) {
+	        this.x = x;
+	        this.y = y;
+	    }
+
+	    public static float length(float x, float y) {
+	        return Math.sqrt(x * x + y * y);
+	    }
+
+	    public static float distance(Vector first, Vector second) {
+	        return length(second.x - first.x, second.y - first.y);
+	    }
+
+	    public static Vector direction(float x, float y) {
+	        return new Vector(x / length(x, y), y / length(x, y));
+	    }
+	}
