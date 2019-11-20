2cd4cb1ed9754a59f736384b6b7f8978688d70db
Aitor Ruiz <aitorruiz2m@gmail.com>
2019-11-20 12:01:53 +0100

    Page update

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.AdministracionDrones"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.administracion"}]
